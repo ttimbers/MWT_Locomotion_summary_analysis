b494add844b57c0fd5a4e0f36daa54802fb7af85
ttimbers <tiffany.timbers@gmail.com>
2015-02-26 15:06:45 -0800

    Planning out our driver script for the project. Tiff, Catrina and Kwangjin.

---
+++ bin/locomotion_driver.sh
@@ -0,0 +1,25 @@
+##This is the driver script that will call modulars scripts to attack each chunk
+##of the problem
+
+##zip all folders all MWT data folders in directory to be analyzed
+##re-use Tiff's script
+
+##optional challenge - copy them to server
+##Tiff
+
+##destroy/delete unzipped MWT folder
+##Kwangjin
+
+##call choreography to analyze the MWT data folders
+##Tiff
+
+##move unzipped folder into a new directory (chore_data)
+##Catrina
+
+##need to create a large file containing all data files with 
+##data, plate name and strain name in each row
+##Catrina
+
+##create figures
+
+##create stats
