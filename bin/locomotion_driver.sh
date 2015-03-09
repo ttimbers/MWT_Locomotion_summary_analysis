e748b75405851cb280501a014ddb33dcb0765387
ttimbers <tiffany.timbers@gmail.com>
2015-03-09 12:34:57 -0700

    Added code to locomotion_driver.sh to save .zipped folders in the data directory toa webdav server whose URL and path to save to is specified by the user. Also added a loop to call chore.jar to analyze MWT .zip's for individual worms to extract D (worm ID), b (direction), s (speed), M (morphwidth), m (midline), e (area), x (x-coordinate), y (y-coordinate) and spontaneous reversals. User must specify location of chore.jar. Idea - perhaps we can try to put the chore.jar file in the path so we don't need to specify this everytime?

--- bin/locomotion_driver.sh
+++ bin/locomotion_driver.sh
@@ -1,30 +1,41 @@
-##This is the driver script that will call modulars scripts to attack each chunk
-##of the problem
+## This is the driver script that will call modulars scripts to attack each chunk
+## of the problem
 ##
-##Set working directory to project's root directory
+## Set working directory to project's root directory
+##
+## Requires the following input from the user:
+##		$1: webdav server URL
+##		$2: path on webdav where .zip folders should be saved
+##		$3: path to chore.jar (offline analys program Choreography)
 
-##zip all folders all MWT data folders in directory to be analyzed
-##must be in "data" working directory 
+## zip all folders all MWT data folders in directory to be analyzed
+## must be in "data" working directory 
 cd data
 for foldername in *; do cd $foldername; zip ../$foldername *; cd ..; done
 cd ..
 
-##optional challenge - copy them to server
-##Tiff
+## destroy/delete unzipped MWT folder
+## Kwangjin
 
-##destroy/delete unzipped MWT folder
-##Kwangjin
+## optional - copy .zip files to a webdav server
+## Note: The mountpoint directory /Volumes/mntpnt/ must be created before 
+## issuing the mount_webdav command. 
+## you will be prompted for your webdav username and password
+mount_webdav -i  $1 /Volumes/mntpnt/
+mv data/20141118_161345.zip /Volumes/mntpnt/$2
 
-##call choreography to analyze the MWT data folders
-##Tiff
+## call choreography to analyze the MWT data (each .zip in the folder data)
+cd data
+for zipfolder in * do java -Xmx8g -jar $3 --shadowless -p 0.027 -M 2 -t 20 -S -N all -o DbsMmexy --plugin Reoutline::despike --plugin Respine --plugin MeasureReversal::all $zipfolder; done
+cd ..
 
-##move unzipped folder into a new directory (chore_data)
-##Catrina
+## move unzipped folder into a new directory (chore_data)
+## Catrina
 
-##need to create a large file containing all data files with 
-##data, plate name and strain name in each row
-##Catrina
+## need to create a large file containing all data files with 
+## data, plate name and strain name in each row
+## Catrina
 
-##create figures
+## create figures
 
-##create stats
+## create stats
