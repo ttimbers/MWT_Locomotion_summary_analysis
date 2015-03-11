86f35acc6542e07d36bb986167a317a36912bf75
ttimbers <tiffany.timbers@gmail.com>
2015-03-11 15:04:19 -0700

    Changes from 2nd working group to get shell part of script working.

--- bin/locomotion_driver.sh
+++ bin/locomotion_driver.sh
@@ -8,51 +8,46 @@
 ##		$2: path on webdav where .zip folders should be saved
 ##		$3: path to chore.jar (offline analys program Choreography)
 
+## Connect to webdav (so you can backup files)
+## you will be prompted for your webdav username and password
+mount_webdav -i  $1 /Volumes
+
 ## zip all folders all MWT data folders in directory to be analyzed
 ## must be in "data" working directory 
 cd data
-for foldername in *; do cd $foldername; zip ../$foldername *; cd ..; done
+for foldername in */; do cd $foldername; zip ../$foldername *; cd ..; done
 cd ..
 
 ## destroy/delete unzipped MWT folder
 ## Kwangjin
-
 cd data
-for A in *
-do
-rm -r $A/*
-rmdir $A
-done
+for A in */; do rm -r $A/; done
+cd ..
 
-## optional - copy .zip files to a webdav server
-## Note: The mountpoint directory /Volumes/mntpnt/ must be created before 
-## issuing the mount_webdav command. 
-## you will be prompted for your webdav username and password
-mount_webdav -i  $1 /Volumes/mntpnt/
-mv data/20141118_161345.zip /Volumes/mntpnt/$2
+## copy .zip files to a webdav server
+cp data/*.zip /Volumes/$2
 
 ## call choreography to analyze the MWT data (each .zip in the folder data)
+## error: Exactly one filename required
+##  Use --help to list valid options.
 cd data
-for zipfolder in * do java -Xmx8g -jar $3 --shadowless -p 0.027 -M 2 -t 20 -S -N all -o DbsMmexy --plugin Reoutline::despike --plugin Respine --plugin MeasureReversal::all $zipfolder; done
+for zipfolder in *.zip; do java -Xmx8g -jar $3 --shadowless -p 0.027 -M 2 -t 20 -S -N all -o DbsMmexy --plugin Reoutline::despike --plugin Respine --plugin MeasureReversal::all $zipfolder; done
 cd ..
 
 ## move unzipped folder into a new directory (chore_data)
 ## Catrina.
-## pwd = data
-## have zip files and choreography folders
-## before running choreography? mkdir chore_data
-## mv *.zip chore_data
-## do choreography in chore_data folder
-## mv chore_data/*.zip .
-
+cd data
+mkdir chore_data
+##figure out how to move a directory ##K & C
+mv */ chore_data
+cd ..
 
 ## need to create a large file containing all data files with 
 ## data, plate name and strain name in each row
-## Catrina
-## cd chore_data
-## cd for each foldername
-## for each .dat file add column with .. name
-## for each .dat file add column with the .dat filename
+##grep -r '[0-9]' $(find ./data -name '*.dat') > merged.file
+cd data/chore_data
+for filename in $(find . -name '*.dat'); do grep -r '[0-9]' $filename >> merged.file; done
+cd ..
 
 ## create figures
 
